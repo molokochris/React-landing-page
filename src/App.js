a680d83556c1cd2c2a2783ea53d4ea9b11ca83dc
molokochris <molokochrisp742@gmail.com>
2023-11-03 11:29:10 +0200

    added href links to mLab and my portfolio website

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import Logo from './components/Logo';
-import './App.css';
-import Menu from './components/Menu';
-import Account from './components/Account';
-import Products from './components/Products';
-import Blog from './components/Blog';
-import About from './components/About';
-import Contact from './components/Contact';
-import Sandwich from './components/Sandwich';
+import Logo from "./components/Logo";
+import "./App.css";
+import Menu from "./components/Menu";
+import Account from "./components/Account";
+import Products from "./components/Products";
+import Blog from "./components/Blog";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Sandwich from "./components/Sandwich";
 // import SideMenu from './components/SideMenu';
 
 function App() {
-  
   return (
     <div className="App">
-    {/* <SideMenu /> */}
+      {/* <SideMenu /> */}
       <div className="head">
         <Sandwich />
         <Logo />
@@ -21,19 +20,30 @@ function App() {
         <Account />
       </div>
       <main>
-        <div className='products-container'>
+        <div className="products-container">
           <Products />
         </div>
-        <div className='others-container'>
+        <div className="others-container">
           <Blog />
-          <div className='more-info'>
+          <div className="more-info">
             <About />
             <Contact />
           </div>
         </div>
       </main>
       <footer>
-        Moloko Chris Poopedi &copy;
+        <a href="https://www.mlab.co.za" target="_blank" rel="noreferrer">
+          mLab CodeTribe
+        </a>{" "}
+        challenge completed by{" "}
+        <a
+          href="https://molokochris.github.io/bootcamp.pw/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Moloko Chris Poopedi
+        </a>{" "}
+        &copy;
       </footer>
     </div>
   );
